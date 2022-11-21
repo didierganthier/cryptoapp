9f4e1d83a14fbba83c0b0c95e71e7ee1d67dc243
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-20 22:47:06 -0500

    Fix ESLint errors

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,12 +3,15 @@ import {
   Typography, Row, Col, Statistic,
 } from 'antd';
 import millify from 'millify';
+import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 const Homepage = () => {
   const { Title } = Typography;
 
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return 'Loading...';
@@ -25,6 +28,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title"> Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title"> Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified />
     </>
   );
 };
